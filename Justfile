bac493e78714402226422abcaf34ff0dd7dec2a5
Roni Hämäläinen <roni.hamalainen@tuni.fi>
2022-12-18 23:37:35 +0200

    Add GPIO example

--- Justfile
+++ Justfile
@@ -11,3 +11,11 @@ hello-asm:
     just hello
     arm-none-eabi-objdump target/armv7a-none-eabi/release/examples/hello -S
     arm-none-eabi-readelf target/armv7a-none-eabi/release/examples/hello -h
+
+gpio:
+    cargo build --release --example gpio
+
+gpio-asm:
+    just gpio
+    arm-none-eabi-objdump target/armv7a-none-eabi/release/examples/gpio -S
+    arm-none-eabi-readelf target/armv7a-none-eabi/release/examples/gpio -h
---
+++ examples/gpio.rs
@@ -0,0 +1,48 @@
+//! GPIO LED blinker for PYNQ-Z1.
+
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+use pynq_z1_bsp::common::instruction::nop;
+use pynq_z1_bsp::peripheral::gpio::PinDirection;
+use pynq_z1_bsp::peripheral::gpio::GPIO;
+use pynq_z1_bsp::peripheral::uart::UART0;
+
+#[no_mangle]
+#[inline(never)]
+#[link_section = ".text"]
+fn main() {
+    unsafe {
+        GPIO.set_mio_direction(0, PinDirection::Input);
+        GPIO.set_mio_direction(1, PinDirection::Output);
+        GPIO.toggle_mio_interrupt(0, true);
+        GPIO.toggle_mio_interrupt(1, true);
+
+        UART0.configure();
+        UART0.toggle(true);
+    }
+
+    loop {
+        for _ in 0..1_000 {
+            nop();
+        }
+        unsafe {
+            if GPIO.read_mio_input(0) {
+                UART0.println("MIO pin 0 is high.");
+            } else {
+                UART0.println("MIO pin 0 is low.");
+            }
+            if GPIO.read_mio_output(1) {
+                GPIO.write_mio_output(1, false);
+            } else {
+                GPIO.write_mio_output(1, true);
+            }
+        }
+    }
+}
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
