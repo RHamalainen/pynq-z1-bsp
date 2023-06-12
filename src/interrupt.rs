75005b3d5d01281cdbc7367bcc68b0b152c5ebf3
Roni Hämäläinen <roni.hamalainen@tuni.fi>
2023-06-12 17:38:12 +0300

    Update interrupt.rs

--- src/interrupt.rs
+++ src/interrupt.rs
@@ -1,4 +1,22 @@
 //! Interrupt functionality.
+//!
+//! # How to use?
+//!
+//! To enable routing and detection of interrupt requests, you have to configure both `GIC` and `ICC`.
+//!
+//! ```ignore
+//! GIC.toggle_enable(false);
+//! ICC.toggle_enable(false);
+//!
+//! GIC.toggle_interrupt_enable(<irq number>, true);
+//! GIC.set_interrupt_targets(<irq number>, 0b1);
+//! GIC.set_interrupt_priority(<irq number>, 0b0);
+//!
+//! ICC.set_interrupt_priority_filter(0b1111_1111);
+//!
+//! ICC.toggle_enable(true);
+//! GIC.toggle_enable(true);
+//! ```
 
 pub mod gic;
 pub mod handler;
