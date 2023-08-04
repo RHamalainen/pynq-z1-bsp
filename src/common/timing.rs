e2bddb420bf97d44cfdd6e1170d809c0fcde200a
Roni Hämäläinen <roni.hamalainen@tuni.fi>
2023-08-04 22:09:58 +0300

    Update timing.rs

--- src/common/timing.rs
+++ src/common/timing.rs
@@ -1,8 +1,10 @@
 //! System timing values.
 
-/// PYNQ-Z1 provides 50 MHz clock to Zynq `PS_CLK` pin.
+/// Processor frequency.
+///
+/// `PYNQ-Z1` provides 50 MHz clock to `Zynq-7000`'s `PS_CLK` pin.
 /// This enables the processor to operate at maximum frequency of 650 MHz.
 pub const FREQUENCY_PROCESSOR: u32 = 650_000_000;
 
-/// Peripherals are clocked half of the processor frequency.
+/// Peripherals are clocked half of the [processor frequency](FREQUENCY_PROCESSOR).
 pub const FREQUENCY_PERIPHERALS: u32 = FREQUENCY_PROCESSOR / 2;
